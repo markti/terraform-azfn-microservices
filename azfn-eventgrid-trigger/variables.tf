1430d864e40ecd05ba3da42b795c0b096a18df22
Mark Tinderholt <mark@vitalsigyn.com>
2020-02-14 00:38:42 -0500

    add eventgrid trigger azfun

---
+++ azfn-eventgrid-trigger/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+    description = "The name used for the Azure Function Resource"
+}
+variable "location" {    
+    description = "The Azure Region to deploy into"
+}
+variable "resource_group_name" {    
+    description = "The Azure Resource Group to deploy into"
+}
+variable "keyvault_id" {    
+    description = "The Key Vault to store endpoint details into"
+}
+variable "tags" {    
+    description = "The Key Vault to store endpoint details into"
+}
