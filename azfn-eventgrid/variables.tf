3142cdd13d7620905fc102aca6c348a5188c46b7
Mark Tinderholt <mark@vitalsigyn.com>
2020-02-13 23:24:47 -0500

    removed types

--- azfn-eventgrid/variables.tf
+++ azfn-eventgrid/variables.tf
@@ -1,16 +1,12 @@
 variable "name" {
-    type = "string"
     description = "The name used for the Azure Function Resource"
 }
 variable "location" {    
-    type = "string"
     description = "The Azure Region to deploy into"
 }
 variable "resource_group_name" {    
-    type = "string"
     description = "The Azure Resource Group to deploy into"
 }
 variable "keyvault_id" {    
-    type = "string"
     description = "The Key Vault to store endpoint details into"
 }
