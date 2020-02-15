465f137854b7f06cc6d8250dbdc4a3c1c327e75b
Mark Tinderholt <mark@vitalsigyn.com>
2020-02-15 16:07:58 -0500

    eventgrid key

--- azfn/main.tf
+++ azfn/main.tf
@@ -41,12 +41,12 @@ resource "azurerm_template_deployment" "azfn_function_key" {
       }
   }
   BODY
-  
+
   depends_on = [azurerm_function_app.function_app]
 }
 
 
-
+/*
 # This will obtain the Azure Function's Key that can be used to integrate with the Azure Function by API Management
 resource "azurerm_template_deployment" "azfn_eventgrid_key" {
   name = "${var.name}-key-rgt"
@@ -78,53 +78,4 @@ resource "azurerm_template_deployment" "azfn_eventgrid_key" {
   BODY
 
   depends_on = [azurerm_function_app.function_app]
-}
-              
-/*
-data "external" "azfn_system_key" {
-	program = ["bash", "-c", "az rest --method post --uri ${var.resource_group_name}/providers/Microsoft.Web/sites/${azurerm_function_app.function_app.name}/host/default/listKeys?api-version=2018-11-01 --query systemKeys"]
-
-	depends_on = [
-		azurerm_function_app.function_app
-	]
-}
-*/
-
-/*
-data "external" "hostKey" {
-	program = ["bash", "-c", "az rest --method post --uri ${azurerm_resource_group.storage_service.id}/providers/Microsoft.Web/sites/${azurerm_function_app.function.name}/host/default/listKeys?api-version=2018-11-01 --query systemKeys"]
-
-	depends_on = [
-		azurerm_function_app.function
-	]
-}*/
-
-/*
-resource "azurerm_template_deployment" "azfn_system_keys" {
-  name = "${var.name}-systemkey-rgt"
-  parameters = {
-    "functionApp" = "${azurerm_function_app.function_app.name}"
-  }
-  resource_group_name    = "${var.resource_group_name}"
-  deployment_mode = "Incremental"
-  
-  template_body   = <<BODY
-  {
-    "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
-    "contentVersion": "1.0.0.0",
-    "parameters": {
-      "functionApp": {"type": "string", "defaultValue": ""}
-    },
-    "variables": {
-      "functionAppId": "[resourceId('Microsoft.Web/sites', parameters('functionApp'))]"
-    },
-    "resources": [],
-    "outputs": {
-      "eventgridKey": {
-        "type": "string",
-        "value": "[listkeys(concat(variables('functionAppId'), '/host/default'), '2018-11-01').systemKeys.eventgridKey]"
-      }
-    }
-  }
-  BODY
 }*/
--- azfn/outputs.tf
+++ azfn/outputs.tf
@@ -1,9 +1,10 @@
 output "function_key" {
   value = "${lookup(azurerm_template_deployment.azfn_function_key.outputs, "functionkey")}"
 }
+/*
 output "eventgrid_key" {
   value = "${lookup(azurerm_template_deployment.azfn_eventgrid_key.outputs, "eventgridkey")}"
-}
+}*/
 
 output "hostname" {
     value = "${azurerm_function_app.function_app.default_hostname}"
