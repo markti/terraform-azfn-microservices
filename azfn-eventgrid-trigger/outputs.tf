4490613d0ab4ce23c6599c3f51a299693a244fe6
Mark Tinderholt <mark@vitalsigyn.com>
2020-02-15 12:15:44 -0500

    removed eg trigger

--- azfn-eventgrid-trigger/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "eventgrid_key" {
-  value = "${lookup(azurerm_template_deployment.function_keys.outputs, "eventGridKey")}"
-}
-
-output "hostname" {
-    value = "${azurerm_function_app.function_app.default_hostname}"
-}
