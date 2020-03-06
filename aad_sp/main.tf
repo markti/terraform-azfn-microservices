cd65a5bc66b6afe706af1c678c6db4042ee09201
Mark Tinderholt <mark@vitalsigyn.com>
2020-03-05 21:39:18 -0500

    basic service principal

---
+++ aad_sp/main.tf
@@ -0,0 +1,40 @@
+
+resource "azuread_application" "aad_app" {
+  name                       = "${var.name}"
+}
+
+resource "random_password" "aad_pwd" {
+  length = 16
+  special = true
+  override_special = "_%@"
+}
+
+resource "azuread_application_password" "aad_app" {
+  application_object_id = "${azuread_application.aad_app.object_id}"
+  value          = "${random_password.aad_app.result}"
+  end_date       = "2021-01-01T01:02:03Z"
+}
+
+resource "azuread_service_principal" "aad_app" {
+  application_id         = "${azuread_application.aad_app.application_id}"
+  
+}
+
+resource "random_password" "aad_sp" {
+  length = 16
+  special = true
+  override_special = "_%@"
+}
+
+resource "azuread_service_principal_password" "aad_sp" {
+  service_principal_id = "${azuread_service_principal.aad_sp.id}"
+  value                = "${random_password.aad_sp.result}"
+  end_date             = "2021-01-01T01:02:03Z"
+}
+
+output "client_id"{
+  value=["${azuread_service_principal.aad_sp.id}"]
+}
+output "client_secret"{
+  value=["${azuread_service_principal_password.aad_sp.value}"]
+}
---
+++ aad_sp/variables.tf
@@ -0,0 +1,4 @@
+variable "name" {
+    type = "string"
+    description = "The name used for the Azure Function Resource"
+}
