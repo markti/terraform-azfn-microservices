e2b0664423dc162c0f0d4d53f92c1c503ce98334
Mark Tinderholt <mark@vitalsigyn.com>
2020-03-12 08:06:34 -0400

    need the object Id for keyvault

--- aad_sp/main.tf
+++ aad_sp/main.tf
@@ -38,3 +38,6 @@ output "client_id"{
 output "client_secret"{
   value="${azuread_service_principal_password.aad_sp.value}"
 }
+output "object_id"{
+  value="${azuread_service_principal.aad_sp.object_id}"
+}
